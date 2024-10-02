4fc1a056beabc3e00ac63050ea4dd90eb596d16c
Alvin_Lim <alchemi@outlook.sg>
2024-10-02 23:41:44 +0800

    done

--- Neo4J_query.py
+++ Neo4J_query.py
@@ -8,21 +8,21 @@ AUTH = ("neo4j", "2kSOQH70unww6GZ52gtrbVHd4N-GNutSH0fbe6nPdj4")
 with GraphDatabase.driver(URI, auth=AUTH) as driver:
     driver.verify_connectivity()
 
-#querying the database
+# querying the database
 
-# Get the name of all 42 year-olds
-records, summary, keys = driver.execute_query(
-    "MATCH (p:Person {age: $age}) RETURN p.name AS name",
-    age=42,
-    database_="neo4j",
-)
+# # Get the name of all 42 year-olds
+# records, summary, keys = driver.execute_query(
+#     "MATCH (p:Person {age: $age}) RETURN p.name AS name",
+#     age=42,
+#     database_="neo4j",
+# )
 
-# Loop through results and do something with them
-for person in records:
-    print(person)
+# # Loop through results and do something with them
+# for person in records:
+#     print(person)
 
-# Summary information
-print("The query `{query}` returned {records_count} records in {time} ms.".format(
-    query=summary.query, records_count=len(records),
-    time=summary.result_available_after,
-))
+# # Summary information
+# print("The query `{query}` returned {records_count} records in {time} ms.".format(
+#     query=summary.query, records_count=len(records),
+#     time=summary.result_available_after,
+# ))
